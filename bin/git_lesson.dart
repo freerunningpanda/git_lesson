dbf5f1252b68e33150713b46ae12037755399006
freerunningpanda <freerunningpanda@yandex.ru>
2025-08-09 16:37:17 +0700

    changes

--- bin/git_lesson.dart
+++ bin/git_lesson.dart
@@ -1,5 +1,3 @@
-import 'package:git_lesson/git_lesson.dart' as git_lesson;
-
 void main(List<String> arguments) {
-  print('Hello world: ${git_lesson.calculate()}!');
+  print('This is the first git lesson');
 }
